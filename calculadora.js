71844c8aa45f4455a38afd2b58e92107b18bf09d
Carlos Mondragon <cmondragonotac@gmail.com>
2025-04-29 18:59:24 -0600

    Agregar nuevas funciones

--- calculadora.js
+++ calculadora.js
@@ -6,5 +6,16 @@ function sumar(a, b) {
     return a - b;
   }
 
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  function dividir(a, b) {
+    if (b === 0) throw new Error("División por cero");
+    return a / b;
+  }
+
   sumar(5,3)
   restar(10,5)
+  multiplicar(10,2)
+  dividir(200/10)
